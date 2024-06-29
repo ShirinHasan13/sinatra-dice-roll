79d3a2a12961e09675d768ae371ac14b0c43f8f1
ShirinHasan13 <shirin10@chicagobooth.edu>
2024-06-29 23:39:29 +0000

    f

--- dice.rb
+++ dice.rb
@@ -6,4 +6,6 @@ get("/zebra")do
 "We must add a route for each path we support"
 end
 require "sinatra/reloader"
-
+get("/giraffe")do
+"Hopefully this shows up without having to restart the server"
+end
