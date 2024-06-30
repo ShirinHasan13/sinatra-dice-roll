e51944235207027d82b6f76d5602e5f34bdadf74
ShirinHasan13 <shirin10@chicagobooth.edu>
2024-06-30 00:56:01 +0000

    d

--- dice.rb
+++ dice.rb
@@ -1,11 +1,29 @@
 require "sinatra"
+require "sinatra/reloader"
+require "better_errors"
+require "binding_of_caller"
+
+use(BetterErrors::Middleware)
+BetterErrors.application_root = __dir__
+BetterErrors::Middleware.allow_ip!('0.0.0.0/0.0.0.0')
+
+
+
 get("/") do
-"Hello world"
+"
+<h1>Dice Roll</h1>
+<ul>
+<li><a href=\"/dice/2/6\">Roll two 6-sided dice</a></li>
+<li><a href=\"/dice/2/10\">Roll two 6-sided dice</a></li>  
+<li><a href=\"/dice/1/20\">Roll two 6-sided dice</a></li> 
+<li><a href=\"/dice/5/4\">Roll two 6-sided dice</a></li> 
+</ul>
+"
 end
 get("/zebra")do
 "We must add a route for each path we support"
 end
-require "sinatra/reloader"
+
 get("/giraffe")do
 "Hopefully this shows up without having to restart the server"
 end
@@ -45,8 +63,3 @@ outcome="You rolled a #{first_die} and a #{second_die} for a total of #{sum}"
 "<h1>5d4</h1>
 <p>#{outcome}</p>"
 end
-require "better_errors"
-require "binding_of_caller"
-use(BetterErrors::Middleware)
-BetterErrors.application_root = __dir__
-BetterErrors::Middleware.allow_ip!('0.0.0.0/0.0.0.0')
