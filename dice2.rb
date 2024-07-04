cf0510a50ebf1f34509de021858d1a066802d844
ShirinHasan13 <shirin10@chicagobooth.edu>
2024-07-04 03:13:45 +0000

    f

---
+++ dice2.rb
@@ -0,0 +1,58 @@
+require "sinatra"
+require "sinatra/reloader"
+require "better_errors"
+require "binding_of_caller"
+
+use(BetterErrors::Middleware)
+BetterErrors.application_root = __dir__
+BetterErrors::Middleware.allow_ip!('0.0.0.0/0.0.0.0')
+
+
+
+get("/") do
+erb(:elephant)
+end
+
+get("/zebra")do
+"We must add a route for each path we support"
+end
+
+get("/giraffe")do
+"Hopefully this shows up without having to restart the server"
+end
+
+
+get("/dice/2/6") do
+  first_die = rand(1..6)
+  second_die = rand(1..6)
+  sum = first_die + second_die
+	
+  outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
+	
+  "<h1>2d6</h1>
+   <p>#{outcome}</p>"
+end
+get("/dice/2/10")do
+first_die = rand(1..6)
+second_die = rand(1..6)
+sum = first_die+second_die
+outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}"
+"<h1>2d10</h1>
+<p>#{outcome}</p>"
+end
+get("/dice/1/20")do
+first_die=rand(1..6)
+second_die=rand(1..6)
+sum=first_die+second_die
+outcome="You rolled a #{first_die} and a #{second_die} for a total of #{sum}"
+"<h1>1d20</h1>
+<p>#{outcome}</p>"
+end
+get("/dice/5/4")do
+first_die=rand(1..6)
+second_die=rand(1..6)
+sum=first_die+second_die
+outcome="You rolled a #{first_die} and a #{second_die} for a total of #{sum}"
+"<h1>5d4</h1>
+<p>#{outcome}</p>"
+end
