492bc26432a84de965851b442f80649009faebdd
ShirinHasan13 <shirin10@chicagobooth.edu>
2024-07-04 04:10:38 +0000

    g

--- dice2.rb
+++ dice2.rb
@@ -35,23 +35,23 @@ get("/dice/2/10")do
 first_die = rand(1..6)
 second_die = rand(1..6)
 sum = first_die+second_die
-outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}"
-"<h1>2d10</h1>
-<p>#{outcome}</p>"
+@outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}"
+
+erb(:two_ten)
+
 end
 get("/dice/1/20")do
 first_die=rand(1..6)
 second_die=rand(1..6)
 sum=first_die+second_die
-outcome="You rolled a #{first_die} and a #{second_die} for a total of #{sum}"
-"<h1>1d20</h1>
-<p>#{outcome}</p>"
+@outcome="You rolled a #{first_die} and a #{second_die} for a total of #{sum}"
+erb(:one_twenty)
 end
+
 get("/dice/5/4")do
 first_die=rand(1..6)
 second_die=rand(1..6)
 sum=first_die+second_die
-outcome="You rolled a #{first_die} and a #{second_die} for a total of #{sum}"
-"<h1>5d4</h1>
-<p>#{outcome}</p>"
+@outcome="You rolled a #{first_die} and a #{second_die} for a total of #{sum}"
+erb(:five_four)
 end
